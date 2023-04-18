2ea2f11aa4c80e1914d1f21721755cf86e76eb5f
alan-ferri <131209678+alan-ferri@users.noreply.github.com>
2023-04-18 19:25:01 -0300

    commit1

---
+++ Visual st ices/Ejercicio de tarea/Ejercicio de tarea/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * Console.WriteLine("Ingresa la clave: ");
+string? clave;
+
+do
+{
+    clave = Console.ReadLine();
+
+} while (clave != "667788");
+
+Console.WriteLine("bien pibe! ");
+*/
+
+
+
+/*
+ * Console.WriteLine("Usuario y calve: ");
+string? clave = Console.ReadLine();
+
+while (clave != "667788")
+{
+    Console.WriteLine("Flaco, esa no es la clave. proba de nuevo");
+    clave = Console.ReadLine();
+}
+*/
+
+/*
+Console.WriteLine("Ingresar un número: ");
+string? num;
+int num_int;
+
+do
+{
+    num = Console.ReadLine();
+    num_int = int.Parse(num);
+} while (num_int <= 0);
+ 
+
+
+Console.WriteLine("ingresaste un positivo");
+*/
+
+/*
+Console.WriteLine("Inserte un numero positivo");
+string? num;
+double num_double;
+double suma_negs = 0;
+int cant_negs = 0;
+do
+{
+    num = Console.ReadLine();
+    num_double = double.Parse(num);
+    if (num_double < 0)
+    {
+        suma_negs += num_double;
+        cant_negs++;
+    }
+} while (num_double <= 0);
+Console.WriteLine("Correcto, ingreso un positivo");
+double promedio = suma_negs / cant_negs;
+Console.WriteLine("El promedio de los numeros negativos ingresados es de: " + promedio);
+*/
+
+/* Console.WriteLine("Ingresar numeros");
+string? numero;
+int numero_entero;
+do
+{
+    numero = Console.ReadLine();
+    numero_entero = int.Parse(numero);
+
+} while (numero_entero < 1000);
+
+Console.WriteLine("correcto");
+*/
+
+/*
+Console.WriteLine("Ingrese un numero con mas de 3 cifras:");
+string? num;
+double num_double;
+double suma = 0;
+int cant_ingresados = 0;
+double promedio = 0;
+do
+{
+    num = Console.ReadLine();
+    num_double = double.Parse(num);
+    suma += num_double;
+    cant_ingresados++;
+    promedio = suma / cant_ingresados;
+    if (num_double < 100)
+    {
+        Console.WriteLine($"El promedio es de {promedio}:");
+    }
+} while (num_double < 1000);
+Console.WriteLine("Perfecto, ingresaste un numero de 4 cifras o mas");
+*/
+
+List<string> ListaString = new List<string>();
+ListaString.Add("Alan");
+
+  
---
+++ Visual st ices/programacionICES/programacionICES/Program.cs
@@ -0,0 +1,91 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Ejercicio para ver la edad 
+ * 
+Console.WriteLine("Ingrese su fecha de nacimiento:");
+string LoQueElTipoPuso = Console.ReadLine();
+//si voy a calcukar mas o menos la edad que tiene tomadno su alo de nac.
+//tendria que calcular asi: 
+int ElAñoDelPibeEnInt = int.Parse(LoQueElTipoPuso);
+
+int LaEdadDelFlaco = 2023 - ElAñoDelPibeEnInt;
+
+Console.WriteLine("Vos tenes o vas a cumplir este año: " + LaEdadDelFlaco);
+
+*/
+
+/* //ejemplo del for para leer 12 valores 
+string LoQueElFlacoPone;
+
+Console.WriteLine("Hola, me das alguna cadena? ");
+LoQueElFlacoPone = Console.ReadLine();
+
+if (LoQueElFlacoPone.Length > 15)
+{ 
+            Console.WriteLine("Mucho texto!!");
+} else
+{
+    Console.WriteLine("bien!! tiene pocos");
+}
+*/
+
+/*
+ * Console.WriteLine("Hola, te voy a ir pidiendo los valores de los 12 meses ");
+//me voy a crear una variable para ir llevando la suma de los montos de cada mes
+int sumita = 0; 
+
+for (int mes = 1; mes <= 12; mes ++)
+{
+    Console.WriteLine("Poneme el valor del mes: " + mes);
+    string valor = Console.ReadLine();
+    sumita = sumita + int.Parse(valor);
+}
+Console.WriteLine("El valor total del año es: " + sumita);
+*/
+
+
+
+/*
+ //este es un ejemplo con while hasta que ponga 0 
+Console.WriteLine("Poneme las ventas y cuando no tengas mas poneme un 0 ");
+Console.WriteLine("ingrese un valor: ");
+int sumita = 0;
+
+String valor = Console.ReadLine();
+int valorInt = int.Parse(valor);
+
+while (valorInt >0)
+{
+    sumita = sumita + valorInt;
+
+    Console.WriteLine("Ingresa valor:  ");
+    valor = Console.ReadLine();
+    valorInt = int.Parse(valor);
+}
+Console.WriteLine("al final de todo vendiste: " + sumita);
+*/
+
+
+
+
+//ejercicio de class
+
+//Ejer: Un alumno ingresará las notas de las 9 materias que tuvo durante el año, mostrar el Promedio de las mismas.
+
+/*
+ * Console.WriteLine("Ingresa tus notas:  ");
+
+int sumita = 0;
+
+for (int nota = 1; nota <= 9; nota++)
+{
+    Console.WriteLine("Ingresa la siguiente nota: " + nota);
+    string valor = Console.ReadLine();
+    sumita = sumita + int.Parse(valor);
+}
+Console.WriteLine("Nota final: " + sumita / 9 );
+*/
+
+
+
+
